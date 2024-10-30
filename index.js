8623e73af8f08faa8490e97368ed10171743a76f
easyryt <developer.easyryt@gmail.com>
2024-10-30 22:03:03 +0530

    kkk

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require("path");
 
 const app = express();
 const port = 3001;
-  
+ 
 app.use(express.json());
-app.use(cors());  
-       
+app.use(cors());   
+        
 app.use(express.static(path.join(__dirname)));
     
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "happyDeepawali.html"));
 }); 
-
+  
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 }); 
